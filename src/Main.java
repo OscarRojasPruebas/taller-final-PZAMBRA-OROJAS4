208c006ad8fb719449ebcc4e308a2efbb403dc80
OscarRojasPruebas <oscar.rojasroa@gmail.com>
2024-08-16 17:32:38 -0500

    Cambio código 4

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         int opcion;
 
         do {
-            System.out.println("==========PAULA =========================");
+            System.out.println("==========OSCAR=========================");
             System.out.println("===================================");
             System.out.println("  MENU CAPITULO DE PROGRAMACION    ");
             System.out.println("===================================");
