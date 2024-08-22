3164044f6b3cab620708ea4f87b0dc402d819f2e
OscarRojasPruebas <oscar.rojasroa@gmail.com>
2024-08-22 11:48:10 -0500

    Modificación código

--- src/Main.java
+++ src/Main.java
@@ -18,23 +18,24 @@ public class Main {
         int opcion;
 
         do {
-            System.out.println("==========OSCAR=========================");
-            System.out.println("===================================");
-            System.out.println("  MENU CAPITULO DE PROGRAMACION    ");
-            System.out.println("===================================");
-            System.out.println("1. Datos Primitivos.");
-            System.out.println("2. String.");
-            System.out.println("3. Constantes.");
-            System.out.println("4. Tipo de operadores.");
-            System.out.println("5. Condicional IF, ELSE IF ELSE.");
-            System.out.println("6. Condicional Switch.");
-            System.out.println("7. Condicional Ternaria.");
-            System.out.println("8. Bucle DO WHILE.");
-            System.out.println("9. Bucle WHILE.");
-            System.out.println("10. Bucle FOR.");
-            System.out.println("11. Salir.");
-            System.out.println("===================================");
-            System.out.print("Digite la su opción: ");
+            System.out.println("==================================================");
+            System.out.println("-        MENÚ CAPÍTULO DE PROGRAMACIÓN           -");
+            System.out.println("-          PAULA ZAMBRANO/OSCAR ROJAS            -");
+            System.out.println("-                 PZAMBRA/OROJAS4                -");
+            System.out.println("==================================================");
+            System.out.println("-          1. Datos Primitivos.                  -");
+            System.out.println("-          2. String.                            -");
+            System.out.println("-          3. Constantes.                        -");
+            System.out.println("-          4. Tipo de operadores.                -");
+            System.out.println("-          5. Condicional IF, ELSE IF ELSE.      -");
+            System.out.println("-          6. Condicional Switch.                -");
+            System.out.println("-          7. Condicional Ternaria.              -");
+            System.out.println("-          8. Bucle DO WHILE.                    -");
+            System.out.println("-          9. Bucle WHILE.                       -");
+            System.out.println("-          10. Bucle FOR.                        -");
+            System.out.println("-          11. Salir.                            -");
+            System.out.println("==================================================");
+            System.out.print("Digite su opción: ");
             opcion = scanner.nextInt();
 
             switch (opcion) {
